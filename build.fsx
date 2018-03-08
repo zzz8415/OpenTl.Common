963ef4d0131b100908eede280a57fbee4cbf9def
Viktor Borisov <vik.borisov@outlook.com>
2018-03-08 21:25:15 +0700

    update build scripts

--- build.fsx
+++ build.fsx
@@ -8,61 +8,59 @@ let buildDir = FullName "./build/"
 let apikey = getBuildParam "apikey"
 let version = getBuildParam "version"
 
-Target "Clean" (fun _ ->
-   CleanDir buildDir
-)
-
-Target "Build" (fun _ -> 
+Target "Default" (fun _ -> 
    XMLHelper.XmlPokeInnerText "./src/OpenTl.Common/OpenTl.Common.csproj" "/Project/PropertyGroup/Version" version
    XMLHelper.XmlPokeInnerText "./src/OpenTl.Common.Testing/OpenTl.Common.Testing.csproj" "/Project/PropertyGroup/Version" version
 
    DotNetCli.Restore (fun p -> p)
 
+   CleanDir buildDir
+ 
    DotNetCli.Build (fun p -> 
    { p with
       Project = "./src/OpenTl.Common/OpenTl.Common.csproj"
       Configuration = "Release"
    })
    
+    DotNetCli.Pack (fun p -> 
+      { p with
+         OutputPath = buildDir
+         Project = "./src/OpenTl.Common/OpenTl.Common.csproj"
+      })
+      
+    Paket.Push (fun nugetParams -> 
+          { nugetParams with
+              ApiKey = apikey
+              WorkingDir = buildDir
+          }
+         )
+   
+   CleanDir buildDir
+
    DotNetCli.Build (fun p -> 
       { p with
          Project = "./src/OpenTl.Common.Testing/OpenTl.Common.Testing.csproj"
          Configuration = "Release"
       })
 
-   ()
-
-   DotNetCli.Pack (fun p -> 
-   { p with
-      OutputPath = buildDir
-      Project = "./src/OpenTl.Common/OpenTl.Common.csproj"
-   })
-   
    DotNetCli.Pack (fun p -> 
       { p with
          OutputPath = buildDir
          Project = "./src/OpenTl.Common.Testing/OpenTl.Common.Testing.csproj"
       })
-)
-
-Target "PublishNuget" (fun _ -> 
+   
    Paket.Push (fun nugetParams -> 
-    { nugetParams with
-        ApiKey = apikey
-        WorkingDir = buildDir
-    }
-   )
+             { nugetParams with
+                 ApiKey = apikey
+                 WorkingDir = buildDir
+             }
+            )
 )
 
 Target "Default" (fun _ ->
    trace "Hello World from FAKE"
 )
 
-// Dependencies
-"Clean"
-   ==> "Build"
-   ==> "PublishNuget"
-   ==> "Default"
 
 // start build
 RunTargetOrDefault "Default"
